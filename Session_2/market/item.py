a9246311c865ffc1929c0e09cbf7f4e7793e8e89
Lars Gabriel <lgabriel23@gmx.de>
2023-09-22 12:02:56 +0000

    Preparion for course september 2023

--- Session_2/market/item.py
+++
@@ -1,8 +0,0 @@
-class Item:
-    def __init__(self, name, price, quantity):
-        self.name = name
-        self.price = price
-        self.quantity = quantity
-
-    def __str__(self):
-        return f"{self.name} (${self.price}): {self.quantity} available"
